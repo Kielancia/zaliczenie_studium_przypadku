9be8698d0fda11c2599cdf6cb002f1053affde5f
Kielancia <136986284+Kielancia@users.noreply.github.com>
2024-11-18 18:49:08 +0100

    Added docs folder
    Docs folder contains html files and index.html

---
+++ docs/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['osoba_0',['Osoba',['../classzadanie__zaliczenie_1_1_osoba.html',1,'zadanie_zaliczenie']]]
+];
---
+++ docs/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['wiek_0',['wiek',['../classzadanie__zaliczenie_1_1_osoba.html#a8c42298b08f8729a7558499fe11e127a',1,'zadanie_zaliczenie::Osoba']]]
+];
